2eeded7ab90cdd6e84f52657bb089b8992888867
RawatDevanshu <rawatdevanshu22@gmail.com>
2024-05-25 12:47:47 +0530

    added song encoding during preprocessing

--- preprocess.py
+++ preprocess.py
@@ -3,6 +3,7 @@ import os
 import music21 as m21
 
 KERN_DATASET_PATH = "./deutschl/test/"
+SAVE_DIR = "./dataset"
 ACCEPTABLE_DURATIONS = [0.25, 0.5, 0.75, 1.0, 1.5, 2, 3, 4]
 
 
@@ -48,6 +49,35 @@ def transpose(song):
     return transposed_song
 
 
+def encode_song(song, time_step=0.25):
+    # p = 60, d=1.0 -> [60, "-", "-", "-"]
+
+    encoded_song = []
+
+    for event in song.flat.notesAndRests:
+
+        # handle notes
+        if isinstance(event, m21.note.Note):
+            symbol = event.pitch.midi
+        # handle rests
+        elif isinstance(event, m21.note.Rest):
+            symbol = "r"
+
+        # convert the note/rest into time series notation
+        steps = int(event.duration.quarterLength / time_step)
+
+        for step in range(steps):
+            if step == 0:
+                encoded_song.append(symbol)
+            else:
+                encoded_song.append("_")
+
+    # cast encoded song to a str
+    encoded_song = " ".join(map(str, encoded_song))
+
+    return encoded_song
+
+
 def preprocess(dataset_path):
     pass
 
@@ -56,15 +86,22 @@ def preprocess(dataset_path):
     songs = load_songs_in_kern(dataset_path)
     print(f"Loaded {len(songs)} songs.")
 
-    for song in songs:
+    for i, song in enumerate(songs):
 
         # filter out songs that have non-acceptable durations
-        if not has_acceptable_durations(song, acceptable_durations):
+        if not has_acceptable_durations(song, ACCEPTABLE_DURATIONS):
             continue
+
         # transpose songs to Cmaj/Amin
         song = transpose(song)
+
         # encode songs with music time series representation
+        encoded_song = encode_song(song)
+
         # save songs to text file
+        save_path = os.path.join(SAVE_DIR, str(i) + ".txt")
+        with open(save_path, "w") as fp:
+            fp.write(encoded_song)
 
 
 if __name__ == "__main__":
@@ -72,9 +109,6 @@ if __name__ == "__main__":
     print(f"Loaded {len(songs)} songs.")
     song = songs[0]
 
-    print(
-        f"Has acceptable duration? {has_acceptable_durations(song, ACCEPTABLE_DURATIONS)}"
-    )
+    preprocess(KERN_DATASET_PATH)
     transposed_song = transpose(song)
-    song.show()
     transposed_song.show()
